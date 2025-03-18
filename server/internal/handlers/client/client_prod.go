86b74e0ebc2ff4d2adfb91f9c7508e535beb51b5
trev <me@trev.xyz>
2025-03-18 17:41:29 -0400

    fix: conditional client build

--- server/internal/handlers/client/client_prod.go
+++ server/internal/handlers/client/client_prod.go
@@ -1,16 +1,24 @@
-// go:build !dev
+//go:build !dev
 
 package client
 
 import (
 	"embed"
+	"io/fs"
+	"log"
 	"net/http"
 )
 
-//go:embed client
+//go:embed all:client
 var eclient embed.FS
 
 func init() {
-	fs := http.FS(eclient)
+	log.Println("Initializing client for production")
+	client, err := fs.Sub(eclient, "client")
+	if err != nil {
+		log.Fatalf("failed to get client: %v", err)
+	}
+
+	fs := http.FS(client)
 	embedfs = &fs
 }
