45072a0e37e8a1e664a14b8d75d41e9387ce93b0
trev <me@trev.xyz>
2025-04-16 03:32:37 -0400

    bump: v0.0.11 -> v0.0.12

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     ...
   }: let
     pname = "trevstack";
-    version = "0.0.11";
+    version = "0.0.12";
 
     supportedSystems = [
       "x86_64-linux"
@@ -104,7 +104,7 @@
       trevstack = pkgs.buildGoModule {
         inherit trevstack-client pname version;
         src = ./server;
-        vendorHash = "sha256-ocOqypV4OjlepoMgYFpk/+zpRzBlHg/dljBVMZzS9Yg=";
+        vendorHash = "sha256-yU+d3bKpMYVkZ9Etqe3cSDtFqswZYZK01d6vxDt/a5U=";
 
         preBuild = ''
           cp -r ${trevstack-client} client
