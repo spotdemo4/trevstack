a5a90a5b7bd5a7c051fe8cd0ccc6e3b09f55a645
trev <me@trev.xyz>
2026-05-13 07:15:45 -0400

    style: set button height, make reset neutral

--- web/lib/form/SubmitButton.tsx
+++ web/lib/form/SubmitButton.tsx
@@ -25,7 +25,7 @@ export const SubmitButton: Component<SubmitButtonProps> = (props) => {
           type="submit"
           disabled={!state().canSubmit || !state().isValid}
           class={twMerge(
-            "inline-flex items-center justify-center rounded-md bg-ctp-sky px-4 py-2 text-sm font-semibold text-ctp-base shadow-sm transition-colors hover:cursor-pointer hover:bg-ctp-sapphire focus:ring-2 focus:ring-ctp-sky/50 focus:ring-offset-2 focus:ring-offset-ctp-mantle focus:outline-none disabled:cursor-not-allowed disabled:bg-ctp-surface1 disabled:text-ctp-overlay0 disabled:shadow-none",
+            "inline-flex h-9.5 items-center justify-center rounded-md bg-ctp-sky px-4 py-2 text-sm font-semibold text-ctp-base shadow-sm transition-colors hover:cursor-pointer hover:bg-ctp-sapphire focus:ring-2 focus:ring-ctp-sky/50 focus:ring-offset-2 focus:ring-offset-ctp-mantle focus:outline-none disabled:cursor-not-allowed disabled:bg-ctp-surface1 disabled:text-ctp-overlay0 disabled:shadow-none",
             props.class,
           )}
         >
